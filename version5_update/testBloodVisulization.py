19e928469bff2e6ba366920ec819376f6c0b4a00
GitBugKing <LZ163163@outlook.com>
2021-12-12 20:29:34 +0000

    BloodVisualization update confirmation

--- version5_update/testBloodVisulization.py
+++ version5_update/testBloodVisulization.py
@@ -1,27 +1,25 @@
 import pygame as gm
 import Status
+import tools
 from constant import w, h
 
-# clock = gm.time.Clock()
 gm.init()
 screen = gm.display.set_mode((w, h))
-# screen = gm.display.get_surface()
 gm.display.set_caption('blood visualization')
 bgpic = gm.image.load("Pictures/backgrounds/bg.jpg").convert()
 bgpicView = gm.transform.scale(bgpic, (w, h))
-blood = gm.image.load("Pictures/backgrounds/bloodvisualization.jpg").convert()
-bloodView = gm.transform.scale(blood, (w/5, h/10))
+bloodView = gm.image.load("Pictures/backgrounds/bloodvisualization.jpg").convert()
+bloodView = tools.get_image(bloodView, 0, 0, 80, 16, (255, 255, 255), h/15/16)
 testrole = Status.Status()
 while True:
     for event in gm.event.get():
         if event.type == gm.QUIT:
             gm.quit()
     screen.blit(bgpicView, (0, 0))
-    screen.blit(bloodView, (0, 0))
-
-    gm.draw.rect(screen, (250, 0, 0), (60, 20, w/30*testrole.blood, h/21), 0)
+    # testrole as mainrole
+    gm.draw.rect(screen, (250, 0, 0), (w/11, h/16, bloodView.get_width()/65*testrole.blood, h/22), 0)
+    screen.blit(bloodView, (w/20, h/20))
     gm.display.update()
-    # clock.tick(60)
 
 
 
